f71191da668503dce7a4d83638fa142a7a34e784
Pranav Kumar <letsgrabitpran@gmail.com>
2024-08-28 00:20:36 +0530

    28082024 creating projects with cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-0cb112254b161a1a.d
@@ -0,0 +1,5 @@
+C:\Users\prakumar180\PROJECTS\RUST\rust\hello_cargo\target\debug\deps\libhello_cargo-0cb112254b161a1a.rmeta: src/main.rs
+
+C:\Users\prakumar180\PROJECTS\RUST\rust\hello_cargo\target\debug\deps\hello_cargo-0cb112254b161a1a.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\prakumar180\PROJECTS\RUST\rust\hello_cargo\target\debug\deps\hello_cargo.exe: src/main.rs
+
+C:\Users\prakumar180\PROJECTS\RUST\rust\hello_cargo\target\debug\deps\hello_cargo.d: src/main.rs
+
+src/main.rs:
