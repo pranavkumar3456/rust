8f6c1239cbb372b5994539234023d719de65bdf0
Pranav Kumar <letsgrabitpran@gmail.com>
2024-08-28 22:47:35 +0530

    adding constants

---
+++ constants/target/debug/constants.d
@@ -0,0 +1,1 @@
+C:\Users\prakumar180\PROJECTS\RUST\rust\constants\target\debug\constants.exe: C:\Users\prakumar180\PROJECTS\RUST\rust\constants\src\main.rs
---
+++ constants/target/debug/deps/constants.d
@@ -0,0 +1,5 @@
+C:\Users\prakumar180\PROJECTS\RUST\rust\constants\target\debug\deps\constants.exe: src/main.rs
+
+C:\Users\prakumar180\PROJECTS\RUST\rust\constants\target\debug\deps\constants.d: src/main.rs
+
+src/main.rs:
