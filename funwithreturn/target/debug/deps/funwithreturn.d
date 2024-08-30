bc500ba742feb8918e7a5c071d727b9e2b9b6b3d
Pranav Kumar <letsgrabitpran@gmail.com>
2024-08-30 23:57:13 +0530

    function with return value

---
+++ funwithreturn/target/debug/deps/funwithreturn.d
@@ -0,0 +1,5 @@
+C:\Users\prakumar180\PROJECTS\RUST\rust\funwithreturn\target\debug\deps\funwithreturn.exe: src/main.rs
+
+C:\Users\prakumar180\PROJECTS\RUST\rust\funwithreturn\target\debug\deps\funwithreturn.d: src/main.rs
+
+src/main.rs:
