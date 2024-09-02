ab4818f2d900d3b9e9f74cd95c1f27c4f64d2b4c
Pranav Kumar <letsgrabitpran@gmail.com>
2024-09-02 09:21:44 +0530

    celsius to Fahrenheit converter

--- temperture_convertor/target/debug/deps/temperture_convertor.d
+++
@@ -1,5 +0,0 @@
-C:\Users\prakumar180\PROJECTS\RUST\rust\temperture_convertor\target\debug\deps\temperture_convertor.exe: src/main.rs
-
-C:\Users\prakumar180\PROJECTS\RUST\rust\temperture_convertor\target\debug\deps\temperture_convertor.d: src/main.rs
-
-src/main.rs:
--- temperture_convertor/target/debug/temperture_convertor.d
+++
@@ -1,1 +0,0 @@
-C:\Users\prakumar180\PROJECTS\RUST\rust\temperture_convertor\target\debug\temperture_convertor.exe: C:\Users\prakumar180\PROJECTS\RUST\rust\temperture_convertor\src\main.rs
